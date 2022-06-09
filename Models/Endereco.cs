0dd33a1305d57c5ad43156d6511558e986f0d8b4
Rafael Lagisck <rafael.lagisck@bebidasasteca.intranet>
2022-06-09 16:50:04 -0300

    Começando da Forma Facil Parte 02

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -1,15 +1,23 @@
-﻿namespace MinhaAppBasica.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinhaAppBasica.Models
 {
     public class Endereco : Entity
     {
         public Guid FornecedorId { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
         public string Logradouro { get; set; }  
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
         public string Numero { get; set; }
 
         public string Complemento { get; set; } 
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(8, ErrorMessage = "O campo {0} precisa ter entre {1}", MinimumLength = 8)]
         public string Cep { get; set; }
 
         public string Bairro { get; set; }
--- Models/Fornecedor.cs
+++ Models/Fornecedor.cs
@@ -1,16 +1,24 @@
-﻿namespace MinhaAppBasica.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MinhaAppBasica.Models
 {
     public class Fornecedor : Entity
     {
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 2)]
         public string Nome { get; set; }
         
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1}", MinimumLength = 11)]
         public string Documento { get; set; }   
         
         public TipoFornecedor TipoFornecedor { get; set; }
 
         public Endereco Endereco { get; set; }
 
+        [DisplayName("Ativo?")]
         public bool Ativo { get; set; }
 
         /*EF Relations*/
