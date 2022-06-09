0fe3ea352a66f5430d30c9fcefd3624235f9d02f
Rafael Lagisck <rafael@meudominio.com>
2022-06-08 22:45:04 -0300

    Começando de Forma Facil Parte 01

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinhaAppBasica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
